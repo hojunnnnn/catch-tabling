057eb631bea22f2051de76a7b82cdb430a4b60c6
HJLee <59757689+hojunnnnn@users.noreply.github.com>
2024-06-22 19:01:04 +0900

    Initial commit

---
+++ src/main/java/com/catchtabling/CatchTablingApplication.java
@@ -0,0 +1,13 @@
+package com.catchtabling;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatchTablingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatchTablingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/catchtabling/CatchTablingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.catchtabling;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatchTablingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

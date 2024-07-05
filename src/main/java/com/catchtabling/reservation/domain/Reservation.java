76b295b63f6be24048d7ca6e3288ee75f990b9e2
HJLee <59757689+hojunnnnn@users.noreply.github.com>
2024-07-05 12:21:06 +0900

    feat : 예약 entity 추가

--- src/main/java/com/catchtabling/reservation/domain/Reservation.java
+++ src/main/java/com/catchtabling/reservation/domain/Reservation.java
@@ -1,6 +1,5 @@
-package com.catchtabling.waiting.domain;
+package com.catchtabling.reservation.domain;
 
-import com.catchtabling.common.domain.BaseTimeEntity;
 import com.catchtabling.member.domain.Member;
 import com.catchtabling.store.domain.Store;
 import jakarta.persistence.*;
@@ -11,31 +10,29 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDateTime;
 
-@NoArgsConstructor
 @Getter
-@Table(name = "REMOTE_WAITING")
+@NoArgsConstructor
+@Table(name = "RESERVATION")
 @Entity
-public class Waiting extends BaseTimeEntity {
+public class Reservation {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @NotNull(message = "대기 번호는 Null 일 수 없습니다.")
-    @Column(name = "wait_number")
-    private Integer waitNumber;
-
     @NotNull(message = "인원 수는 Null 일 수 없습니다.")
     @Column(name = "visitor_cnt")
     private Integer visitorCount;
 
-    // 0: 대기 1: 확정대기, 2: 입장완료, 3: 취소
     @NotNull(message = "상태는 Null 일 수 없습니다.")
-    @Column(name = "status")
     private Integer status;
 
-    @Column(name = "notified_date")
-    private LocalDateTime notifiedDate;
+    @Column(name = "request_memo")
+    private String requestMemo;
+
+    @NotNull(message = "예약 일시는 Null 일 수 습니다.")
+    @Column(name = "reservation_date")
+    private LocalDateTime reservationDate;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "store_info_id")
@@ -46,12 +43,11 @@ public class Waiting extends BaseTimeEntity {
     private Member member;
 
     @Builder
-    public Waiting(Integer waitNumber, Integer visitorCount, Integer status, LocalDateTime notifiedDate, Store store, Member member) {
-        // validate(dosomething..);
-        this.waitNumber = waitNumber;
+    public Reservation(Integer visitorCount, Integer status, String requestMemo, LocalDateTime reservationDate, Store store, Member member) {
         this.visitorCount = visitorCount;
         this.status = status;
-        this.notifiedDate = notifiedDate;
+        this.requestMemo = requestMemo;
+        this.reservationDate = reservationDate;
         this.store = store;
         this.member = member;
     }
